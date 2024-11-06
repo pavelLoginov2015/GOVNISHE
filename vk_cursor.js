f9b1982626d16596bcbc126d98c1f602b652d2b5
pavelLoginov2015 <ytfmmyt123@gmail.com>
2024-11-06 17:20:57 +0500

    not vk_jsfile

--- vk_cursor.js
+++
@@ -1,9 +0,0 @@
-const oRequestPointerLock = Element.prototype.requestPointerLock;
-Element.prototype.requestPointerLock = async function hkRequestPointerLock(...args) {
-	try {
-		(args[0] ||= {}).unadjustedMovement = true;
-		await oRequestPointerLock.apply(this, args);
-	} catch (err) {
-		console.log(err);
-	}
-};
